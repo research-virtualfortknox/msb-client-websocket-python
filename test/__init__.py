d5ee7570e533d7ac350ded95f7bbeb045c2554e2
maas <Matthias.Stoehr@ipa.fraunhofer.de>
2019-06-07 10:43:39 +0200

    Added init file to solve coverage reporting
    It seems that pytest-cov in travis and its virtual env needs the init file to collect the coverage correctly

---
+++ test/__init__.py
@@ -0,0 +1,7 @@
+"""
+Copyright (c) 2019 Fraunhofer Institute for Manufacturing Engineering and Automation (IPA)
+Authors: Daniel Stock, Matthias Stoehr
+
+Licensed under the Apache License, Version 2.0
+See the file "LICENSE" for the full license governing this code.
+"""
