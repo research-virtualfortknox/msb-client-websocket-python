807cafae2feda03f3bf0c173b74f67c3bbcdef3a
maas <Matthias.Stoehr@ipa.fraunhofer.de>
2023-10-20 14:49:05 +0200

    set version to 1.0.16

--- msb_client/__init__.py
+++ msb_client/__init__.py
@@ -5,7 +5,7 @@ Authors: Daniel Stock, Matthias Stoehr
 Licensed under the Apache License, Version 2.0
 See the file "LICENSE" for the full license governing this code.
 """
-__version__ = "1.0.15"
+__version__ = "1.0.16"
 
 # from .ComplexDataFormat import ComplexDataFormat
 # from .DataFormat import DataFormat
